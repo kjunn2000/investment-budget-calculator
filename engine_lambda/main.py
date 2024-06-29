012d32fbe4ba62df3d41d38f57a07ce716c51b6e
Kai <kjunn2000@gmail.com>
2024-06-29 18:25:28 +0800

    add lambda handler

--- engine_lambda/main.py
+++ engine_lambda/main.py
@@ -5,12 +5,33 @@ from datetime import datetime, timedelta
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+def handler(event, _):
+    try:
 
-def download_tickets(tickets: list):
+        body = event.get("body")
+        latest_price = _download_tickets(body.keys())
+        total_price = _calculate_latest_price(latest_price, body)
+        return {
+            "statusCode": 200,
+            "body": total_price
+        }
+    except Exception as e:
+        logger.error(e)
+        return {
+            "statusCode": 500,
+            "body": "Internal Server Error"
+        }
+
+
+def _download_tickets(tickets: list):
     ticketStr = " ".join(tickets)
     yesterday = (datetime.now() - timedelta(days=1)).strftime("%Y-%m-%d")
     response = yf.download(ticketStr, start=yesterday, rounding=True)
-    return _extract_ticket(response, tickets[0]) if len(tickets) == 1 else _extract_tickets(response, tickets)
+    return (
+        _extract_ticket(response, tickets[0])
+        if len(tickets) == 1
+        else _extract_tickets(response, tickets)
+    )
 
 
 def _extract_ticket(market_data, ticket):
@@ -18,4 +39,11 @@ def _extract_ticket(market_data, ticket):
 
 
 def _extract_tickets(market_data, tickets):
-    return { ticket: market_data["Close"][ticket].iloc[-1] for ticket in tickets}
+    return {ticket: market_data["Close"][ticket].iloc[-1] for ticket in tickets}
+
+
+def _calculate_latest_price(latest_price, quantity_dict):
+    return {
+        ticket: latest_price[ticket] * quantity_dict[ticket]
+        for ticket in quantity_dict.keys()
+    }
--- tests/test_download_tickets.py
+++ tests/test_download_tickets.py
@@ -1,66 +1,111 @@
 from numpy import float64
-from engine_lambda.main import download_tickets, _extract_ticket, _extract_tickets
+from engine_lambda.main import (
+    _download_tickets,
+    _extract_ticket,
+    _extract_tickets,
+    _calculate_latest_price,
+    handler,
+)
 import numpy as np
 from unittest.mock import ANY
 import pandas as pd
 
+mock_latest_price = {"SPY": 300, "AAPL": 350}
+
+mock_total_price = {"SPY": np.float64(600), "AAPL": np.float64(1400)}
+
+mock_quantity_dict = {"SPY": 2, "AAPL": 4}
+
+
 class TestDownloadTicket:
 
     def test_download_one_ticket(self, mocker):
-        input_ticket_numbers = ['SPY']
+        input_ticket_numbers = ["SPY"]
         mocker.patch("engine_lambda.main.yf.download")
-        mock_extract_ticket = mocker.patch("engine_lambda.main._extract_ticket", return_value = {
-            'SPY': np.float64(300)
-        })
+        mock_extract_ticket = mocker.patch(
+            "engine_lambda.main._extract_ticket", return_value={"SPY": np.float64(300)}
+        )
 
-        response = download_tickets(input_ticket_numbers)
-        
+        response = _download_tickets(input_ticket_numbers)
+
         assert response is not None
-        assert type(response['SPY']) == float64
-        mock_extract_ticket.assert_called_once_with(ANY, 'SPY')
-
-
+        assert type(response["SPY"]) == float64
+        mock_extract_ticket.assert_called_once_with(ANY, "SPY")
 
     def test_download_multiple_tickes(self, mocker):
-        input_ticket_numbers = ['SPY','NVDA']
+        input_ticket_numbers = ["SPY", "NVDA"]
         mocker.patch("engine_lambda.main.yf.download")
 
-        mock_extract_tickets = mocker.patch("engine_lambda.main._extract_tickets", return_value = {
-            'SPY': np.float64(300),
-            'NVDA': np.float64(300)
-        })
+        mock_extract_tickets = mocker.patch(
+            "engine_lambda.main._extract_tickets",
+            return_value={"SPY": np.float64(300), "NVDA": np.float64(300)},
+        )
 
-        response = download_tickets(input_ticket_numbers)
-        
+        response = _download_tickets(input_ticket_numbers)
+
         assert response is not None
-        assert type(response['SPY']) == float64
-        assert type(response['NVDA']) == float64
+        assert type(response["SPY"]) == float64
+        assert type(response["NVDA"]) == float64
         mock_extract_tickets.assert_called_once_with(ANY, input_ticket_numbers)
-        
+
     def test_extract_ticket(self):
         data = {
-            "Close": pd.Series([100, 200, 300], index=["2023-01-01", "2023-01-02", "2023-01-03"])
+            "Close": pd.Series(
+                [100, 200, 300], index=["2023-01-01", "2023-01-02", "2023-01-03"]
+            )
         }
         market_data = pd.DataFrame(data)
         ticket = "SPY"
         expected = {ticket: 300}
-        
+
         result = _extract_ticket(market_data, ticket)
-        
+
         assert result == expected
 
     def test_extract_tickets(self):
-        market_data = pd.DataFrame({
-            "SPY": [100, 200, 300],
-            "AAPL": [150, 250, 350]
-        }, index=["2023-01-01", "2023-01-02", "2023-01-03"])
+        market_data = pd.DataFrame(
+            {"SPY": [100, 200, 300], "AAPL": [150, 250, 350]},
+            index=["2023-01-01", "2023-01-02", "2023-01-03"],
+        )
         tickets = ["SPY", "AAPL"]
-        expected = {
-            "SPY": 300,
-            "AAPL": 350
-        }
+        expected = mock_latest_price
         wrapped_market_data = {"Close": market_data}
-        
+
         result = _extract_tickets(wrapped_market_data, tickets)
 
         assert result == expected
+
+    def test_calculate_price(self):
+        response = _calculate_latest_price(mock_latest_price, mock_quantity_dict)
+
+        assert response == mock_total_price
+
+    def test_handler_with_one_ticket(self, mocker):
+        event = {"body": {"SPY": 2, "AAPL": 4}}
+        mock_download = mocker.patch(
+            "engine_lambda.main._download_tickets", return_value=mock_latest_price
+        )
+        mock_calculate_price = mocker.patch(
+            "engine_lambda.main._calculate_latest_price", return_value=mock_total_price
+        )
+
+        response = handler(event, {})
+
+        mock_download.assert_called_once_with(event.get("body").keys())
+        mock_calculate_price.assert_called_once_with(
+            mock_latest_price, mock_quantity_dict
+        )
+        assert response["statusCode"] == 200
+        assert response["body"] == mock_total_price
+        
+    def test_handler_exception(self, mocker):
+        event = {"body": {"SPY": 2, "AAPL": 4}}
+        mock_download = mocker.patch(
+            "engine_lambda.main._download_tickets", side_effect=Exception("Error")
+        )
+
+        response = handler(event, {})
+
+        mock_download.assert_called_once_with(event.get("body").keys())
+        assert response["statusCode"] == 500
+        assert response["body"] == "Internal Server Error"
